49ee496891a85c337608c8ef50c3d534054a0c37
Daniel Salinas <daniel@Daniels-MacBook-Pro.local>
2022-12-04 13:14:35 -0500

    Remove copy pasta

--- import/src/graphql/client.ts
+++ import/src/graphql/client.ts
@@ -1,12 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
-import * as functions from 'firebase-functions';
 
 import { getSdk } from './schema';
 
 const client = new GraphQLClient(process.env.API_ENDPOINT || '', {
   headers: {
     'Content-Type': 'application/json',
-    'X-Hasura-Admin-Secret': process.env.MASTER_KEY || functions.config().hasura.admin_secret,
+    'X-Hasura-Admin-Secret': process.env.MASTER_KEY || '',
   },
 });
 
