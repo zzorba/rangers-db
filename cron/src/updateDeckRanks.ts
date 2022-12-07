955fc5810adac602b9d08bb693cd620c7e02ce1a
Daniel Salinas <daniel@Daniels-MacBook-Pro.local>
2022-12-06 21:15:48 -0500

    Add log error messages

--- cron/src/updateDeckRanks.ts
+++ cron/src/updateDeckRanks.ts
@@ -6,9 +6,11 @@ async function refreshDecks() {
     throw new Error('No connection string!');
   }
   const client = new Client(connectionString);
+  console.log('Starting update of deck ranks.');
   await client.connect();
   await client.query('REFRESH MATERIALIZED VIEW rangers.deck_rank;')
   await client.end();
+  console.log('Success, time to sleep.')
 }
 
 refreshDecks();
