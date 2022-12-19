8819e53d78d6b34cc476f4dc8cae58d37f5ffb82
Daniel Salinas <daniel@Daniels-MacBook-Pro.local>
2022-12-19 15:48:21 -0600

    Clean up description

--- frontend/src/components/DeckDetail.tsx
+++ frontend/src/components/DeckDetail.tsx
@@ -133,132 +133,134 @@ export default function Deck({ deck, cards }: Props & { cards: CardsMap }) {
 
   return (
     <>
-      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
-        <GridItem colSpan={deck.description ? [6, 6, 4] : 6}>
-          <Box>
-            <Box paddingTop="2rem" paddingBottom="2em">
-              <Flex direction="row" justifyContent="space-between">
-                <Flex direction="column">
-                  <Heading>{deck?.name || 'Deck'}</Heading>
-                  { authUser?.uid !== deck.user_id && deck.user.handle && (
-                    <Text fontSize="lg">
-                      <CoreIcon icon="ranger" size={18}/>&nbsp;{deck.user.handle}
-                    </Text>
-                  ) }
-                  { !!deck.campaign && <Flex direction="row" alignItems="center"><CoreIcon icon="guide" size={18} /><Link marginLeft={1} as={NextLink} href={`/campaigns/${deck.campaign.id}`}>{deck.campaign.name}</Link></Flex>}
-                  <DeckCountLine parsedDeck={parsedDeck} />
-                </Flex>
-                { authUser && (
-                  <SimpleGrid columns={deck.description ? 2 : [2, 2, 2, 4]}
-                    spacingX={2} spacingY={2} paddingTop={2} paddingBottom={4}>
-                    { authUser.uid === deck.user_id && !deck.next_deck && (
-                      <SolidButton
-                        color="blue"
-                        leftIcon={<EditIcon />}
-                        as={NextLink}
-                        href={`/decks/edit/${deck.id}`}
-                      >
-                        {t`Edit`}
-                      </SolidButton>
-                    ) }
-                    { authUser.uid === deck.user_id && !deck.next_deck && (
-                      <Tooltip label={!!deck.meta.problem ? t`You must correct deck errors before upgrading.` : t`Choosing to camp will make a copy of your deck to let you track deck changes as you play through a campaign.`}>
-                        <SolidButton
-                          color="green"
-                          leftIcon={<ArrowUpIcon />}
-                          onClick={onUpgradeDeck}
-                          isLoading={upgrading}
-                          disabled={!!deck.meta.problem}
-                        >
-                          {t`Camp`}
-                        </SolidButton>
-                      </Tooltip>
-                    ) }
-                    { !deck.previous_deck && (
-                      <Tooltip label={authUser.uid === deck.user_id ?
-                          t`Duplicate this deck while preserving the original.` :
-                          t`Copy this deck to make your own changes.`}>
-                        <SolidButton
-                          color="orange"
-                          leftIcon={<CopyIcon />}
-                          onClick={onCopyDeck}
-                          isLoading={copying}
-                        >
-                          {t`Copy`}
-                        </SolidButton>
-                      </Tooltip>
-                    ) }
-                    { authUser.uid === deck.user_id && !deck.next_deck && (
-                      <SolidButton
-                        color="red"
-                        leftIcon={<DeleteIcon />}
-                        onClick={onDeleteClick}
-                        isLoading={copying}
-                      >
-                        {t`Delete`}
-                      </SolidButton>
-                    ) }
-                  </SimpleGrid>
-                ) }
-              </Flex>
-            </Box>
-            <DeckDescription deck={deck} />
-            { !!parsedDeck.role ? (
-              <ChosenRole role={parsedDeck.role} showCard={showCard} />
-            ) : (
-              <Text>
-                <i>{categories.specialty?.name}:&nbsp;</i>
-                {
-                  specialty && categories.specialty ?
-                  categories.specialty.options[specialty] :
-                  t`Not set`
-                }
-              </Text>
-            ) }
-            { !!parsedDeck.problem?.length && hasCards && (
-              <Box marginTop={2} marginBottom={2}>
-                <DeckProblemComponent
-                  limit={1}
-                  summarizeOthers
-                  errors={parsedDeck.problem}
-                />
-              </Box>
-            ) }
-            <Flex direction="row" maxW="24rem" marginTop={2}>
-              <AspectCounter aspect={AWA} count={deck.awa} />
-              <AspectCounter aspect={SPI} count={deck.spi} />
-              <AspectCounter aspect={FIT} count={deck.fit} />
-              <AspectCounter aspect={FOC} count={deck.foc} />
+      <Box>
+        <Box paddingTop="2rem" paddingBottom="2em">
+          <Flex direction="row" justifyContent="space-between">
+            <Flex direction="column">
+              <Heading>{deck?.name || 'Deck'}</Heading>
+              { authUser?.uid !== deck.user_id && deck.user.handle && (
+                <Text fontSize="lg">
+                  <CoreIcon icon="ranger" size={18}/>&nbsp;{deck.user.handle}
+                </Text>
+              ) }
+              { !!deck.campaign && <Flex direction="row" alignItems="center"><CoreIcon icon="guide" size={18} /><Link marginLeft={1} as={NextLink} href={`/campaigns/${deck.campaign.id}`}>{deck.campaign.name}</Link></Flex>}
+              <DeckCountLine parsedDeck={parsedDeck} />
             </Flex>
-            { hasCards ? (
-              <List>
-                {map(parsedDeck.cards, item => <DeckItemComponent lightCount key={item.id} item={item} showCard={showCard} />)}
-              </List>
-            ) : <Spinner size="md" /> }
-            { (!!deck.previous_deck || !!deck.next_deck) && (
-              <Flex direction="column" marginTop={8}>
-                <Text borderBottomWidth={1} borderColor="gray.500" marginBottom={2} paddingBottom={1} fontWeight="600">{t`Campaign progress`}</Text>
-                { !!deck.previous_deck && (
-                  <Link as={NextLink} href={`/decks/view/${deck.previous_deck.id}`}>{t`Previous deck (${deck.previous_deck.version})`}</Link>
-                )}
-                { !!deck.next_deck && (
-                  <Link as={NextLink} href={`/decks/view/${deck.next_deck.id}`}>{t`Next deck (${deck.next_deck.version})`}</Link>
-                )}
+            { authUser && (
+              <SimpleGrid columns={[2, 2, 2, 4]}
+                spacingX={2} spacingY={2} paddingTop={2} paddingBottom={4}>
+                { authUser.uid === deck.user_id && !deck.next_deck && (
+                  <SolidButton
+                    color="blue"
+                    leftIcon={<EditIcon />}
+                    as={NextLink}
+                    href={`/decks/edit/${deck.id}`}
+                  >
+                    {t`Edit`}
+                  </SolidButton>
+                ) }
+                { authUser.uid === deck.user_id && !deck.next_deck && (
+                  <Tooltip label={!!deck.meta.problem ? t`You must correct deck errors before upgrading.` : t`Choosing to camp will make a copy of your deck to let you track deck changes as you play through a campaign.`}>
+                    <SolidButton
+                      color="green"
+                      leftIcon={<ArrowUpIcon />}
+                      onClick={onUpgradeDeck}
+                      isLoading={upgrading}
+                      disabled={!!deck.meta.problem}
+                    >
+                      {t`Camp`}
+                    </SolidButton>
+                  </Tooltip>
+                ) }
+                { !deck.previous_deck && (
+                  <Tooltip label={authUser.uid === deck.user_id ?
+                      t`Duplicate this deck while preserving the original.` :
+                      t`Copy this deck to make your own changes.`}>
+                    <SolidButton
+                      color="orange"
+                      leftIcon={<CopyIcon />}
+                      onClick={onCopyDeck}
+                      isLoading={copying}
+                    >
+                      {t`Copy`}
+                    </SolidButton>
+                  </Tooltip>
+                ) }
+                { authUser.uid === deck.user_id && !deck.next_deck && (
+                  <SolidButton
+                    color="red"
+                    leftIcon={<DeleteIcon />}
+                    onClick={onDeleteClick}
+                    isLoading={copying}
+                  >
+                    {t`Delete`}
+                  </SolidButton>
+                ) }
+              </SimpleGrid>
+            ) }
+          </Flex>
+        </Box>
+        <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+          <GridItem colSpan={deck.description ? [6, 6, 4] : 6}>
+            <Box borderRightWidth={deck.description ? '1px' : undefined} paddingRight={deck.description ? 6 : undefined}>
+              <DeckDescription deck={deck} />
+              { !!parsedDeck.role ? (
+                <ChosenRole role={parsedDeck.role} showCard={showCard} />
+              ) : (
+                <Text>
+                  <i>{categories.specialty?.name}:&nbsp;</i>
+                  {
+                    specialty && categories.specialty ?
+                    categories.specialty.options[specialty] :
+                    t`Not set`
+                  }
+                </Text>
+              ) }
+              { !!parsedDeck.problem?.length && hasCards && (
+                <Box marginTop={2} marginBottom={2}>
+                  <DeckProblemComponent
+                    limit={1}
+                    summarizeOthers
+                    errors={parsedDeck.problem}
+                  />
+                </Box>
+              ) }
+              <Flex direction="row" maxW="24rem" marginTop={2}>
+                <AspectCounter aspect={AWA} count={deck.awa} />
+                <AspectCounter aspect={SPI} count={deck.spi} />
+                <AspectCounter aspect={FIT} count={deck.fit} />
+                <AspectCounter aspect={FOC} count={deck.foc} />
               </Flex>
-            )}
-          </Box>
-        </GridItem>
-        { !!deck.description && (
-          <GridItem colSpan={[6, 6, 2]}>
-            <Stack borderLeftWidth="1px" marginTop="1em" paddingLeft={2}>
-              <Text fontSize="2xl" fontWeight="600">
-                {t`Description`}
-              </Text>
-              <DeckDescriptionView description={deck.description} />
-            </Stack>
+              { hasCards ? (
+                <List>
+                  {map(parsedDeck.cards, item => <DeckItemComponent lightCount key={item.id} item={item} showCard={showCard} />)}
+                </List>
+              ) : <Spinner size="md" /> }
+            </Box>
           </GridItem>
-        ) }
-      </Grid>
+            { !!deck.description && (
+            <GridItem colSpan={[6, 6, 2]}>
+              <Stack>
+                <Text fontSize="2xl" fontWeight="600">
+                  {t`Description`}
+                </Text>
+                <DeckDescriptionView description={deck.description} />
+              </Stack>
+            </GridItem>
+          ) }
+        </Grid>
+        { (!!deck.previous_deck || !!deck.next_deck) && (
+          <Flex direction="column" marginTop={8}>
+            <Text borderBottomWidth={1} borderColor="gray.500" marginBottom={2} paddingBottom={1} fontWeight="600">{t`Campaign progress`}</Text>
+            { !!deck.previous_deck && (
+              <Link as={NextLink} href={`/decks/view/${deck.previous_deck.id}`}>{t`Previous deck (${deck.previous_deck.version})`}</Link>
+            )}
+            { !!deck.next_deck && (
+              <Link as={NextLink} href={`/decks/view/${deck.next_deck.id}`}>{t`Next deck (${deck.next_deck.version})`}</Link>
+            )}
+          </Flex>
+        )}
+      </Box>
       {cardModal}
       {deleteDialog}
     </>
