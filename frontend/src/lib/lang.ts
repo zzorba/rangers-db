2de39603a4dcb4d3f64d30a0ea0a2466d8195ad5
Daniel Salinas <daniel@daniels-mbp.myfiosgateway.com>
2024-01-31 12:03:18 -0500

    RU plurals

--- frontend/src/lib/lang.ts
+++ frontend/src/lib/lang.ts
@@ -5,11 +5,23 @@ export function getPlural(lang: string, plurals: string, count: number) {
     case 'de':
     case 'it':
     case 'fr':
-    case 'ru':
-      default:
+    default: {
       if (count !== 1 && plurals.length) {
         return p[1];
       }
       return p[0];
+    }
+    case 'ru': {
+      if (plurals.length !== 3) {
+        return plurals[0];
+      }
+      if (count % 10 === 1 && count % 100 !== 11) {
+        return p[0];
+      }
+      if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
+        return p[1];
+      }
+      return p[2];
+    }
   }
 }
